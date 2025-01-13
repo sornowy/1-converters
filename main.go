8485d755817f4c84a00781e5670026f22619a837
Timur Kadyrov <sornowy@gmail.com>
2025-01-14 02:04:50 +0700

    ДЗ Строки и функции

--- main.go
+++ main.go
@@ -4,8 +4,21 @@ import "fmt"
 
 func main() {
 	//Конвертация из USD в EUR
-	const convertEUR = 0.97
-	const convertRUB = 104.55
-	const convertEURtoRUB = convertRUB / convertEUR
-	fmt.Print(convertEURtoRUB)
+	const convertUSDtoEUR = 0.97
+	const convertUSDtoRUB = 104.55
+	const convertEURtoRUB = convertUSDtoRUB / convertUSDtoEUR
+	fmt.Println(convertEURtoRUB)
+	fmt.Print(getUserInput())
+}
+
+func getUserInput() float64 {
+	var value float64
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&value)
+	return value
+}
+
+func calculateCurrency(convertUSDtoEUR, convertUSDtoRUB, value float64) float64 {
+	const convertUSDtoEUR = 0.97
+	const convertUSDtoRUB = 104.55
 }
